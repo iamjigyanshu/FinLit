11de5019bf9228615038a879ff91ceef37985a56
iamjigyanshu <78719108+iamjigyanshu@users.noreply.github.com>
2022-09-30 20:10:04 +0530

    Update login_test.py

--- Login_Test/login_test.py
+++ Login_Test/login_test.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
+import yfinance as yf
+import plotly.graph_objects as go
 
 
 # Security
@@ -36,42 +39,42 @@ def view_all_users():
 	data = c.fetchall()
 	return data
 
+
 def analytics():
-	
-	#s&p500.csv
+	# s&p500.csv
 	ticker_list = pd.read_csv('s&p500.csv')
 	selected_stock = st.sidebar.selectbox('Stock', ticker_list)
 
 	stock_data = yf.Ticker(selected_stock)
 
-	df = stock_data.history(period = 'max')
+	df = stock_data.history(period='max')
 	df.reset_index(inplace=True)
 
 	fig = go.Figure([go.Scatter(x=df['Date'], y=df['High'])])
 	fig.update_xaxes(
-	rangeslider_visible=True,
-	rangeselector=dict(
-	    buttons=list([
-		dict(count=1, label="1m", step="month",                                        
-		     stepmode="backward"),
-		dict(count=6, label="6m", step="month",  
-		     stepmode="backward"),
-		dict(count=1, label="YTD", step="year", 
-		     stepmode="todate"),
-		dict(count=1, label="1y", step="year", 
-		     stepmode="backward"),
-		dict(step="all")
-	    ])
-	)
+		rangeslider_visible=True,
+		rangeselector=dict(
+			buttons=list([
+				dict(count=1, label="1m", step="month",
+					 stepmode="backward"),
+				dict(count=6, label="6m", step="month",
+					 stepmode="backward"),
+				dict(count=1, label="YTD", step="year",
+					 stepmode="todate"),
+				dict(count=1, label="1y", step="year",
+					 stepmode="backward"),
+				dict(step="all")
+			])
+		)
 	)
 	st.plotly_chart(fig, use_container_width=True)
 
 
 
 def main():
-	"""FinLit"""
+	"""Simple Login App"""
 
-	st.title("FinLit")
+	st.title("Simple Login App")
 
 	menu = ["Home","Login","SignUp"]
 	choice = st.sidebar.selectbox("Menu",menu)
@@ -94,7 +97,7 @@ def main():
 
 				st.success("Logged In as {}".format(username))
 
-				task = st.selectbox("Task",["Profile","Analytics","Financial Literacy"])
+				task = st.selectbox("Task",["Add Post","Analytics","Profiles"])
 				if task == "Add Post":
 					st.subheader("Add Your Post")
 
