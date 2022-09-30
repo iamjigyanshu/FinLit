3a2d77f373bd2a58d6e347f91231f8897e8bf411
iamjigyanshu <78719108+iamjigyanshu@users.noreply.github.com>
2022-09-30 20:04:02 +0530

    Update login_test.py

--- Login_Test/login_test.py
+++ Login_Test/login_test.py
@@ -36,6 +36,34 @@ def view_all_users():
 	data = c.fetchall()
 	return data
 
+def analytics():
+    ticker_list = pd.read_csv('s&p500.csv')
+    selected_stock = st.sidebar.selectbox('Stock', ticker_list)
+    
+    stock_data = yf.Ticker(selected_stock)
+    
+    df = stock_data.history(period = 'max')
+    df.reset_index(inplace=True)
+    
+    fig = go.Figure([go.Scatter(x=df['Date'], y=df['High'])])
+    fig.update_xaxes(
+        rangeslider_visible=True,
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1, label="1m", step="month",                                        
+                     stepmode="backward"),
+                dict(count=6, label="6m", step="month",  
+                     stepmode="backward"),
+                dict(count=1, label="YTD", step="year", 
+                     stepmode="todate"),
+                dict(count=1, label="1y", step="year", 
+                     stepmode="backward"),
+                dict(step="all")
+            ])
+        )
+    )
+    st.plotly_chart(fig, use_container_width=True)
+
 
 
 def main():
@@ -70,6 +98,7 @@ def main():
 
 				elif task == "Analytics":
 					st.subheader("Analytics")
+					analytics()
 				elif task == "Profiles":
 					st.subheader("User Profiles")
 					user_result = view_all_users()
