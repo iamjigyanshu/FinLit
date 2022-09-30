d3d9db817b173538562b10134aa35a346e60624d
iamjigyanshu <78719108+iamjigyanshu@users.noreply.github.com>
2022-09-30 21:23:31 +0530

    Update Analytics_test.py

--- Login_Test/Analytics_test.py
+++ Login_Test/Analytics_test.py
@@ -4,30 +4,31 @@ import plotly.express as px
 import yfinance as yf
 import plotly.graph_objects as go
 
-# s&p500.csv
-ticker_list = pd.read_csv('s&p500.csv')
-selected_stock = st.sidebar.selectbox('Stock', ticker_list)
+def app():
+	# s&p500.csv
+	ticker_list = pd.read_csv('s&p500.csv')
+	selected_stock = st.sidebar.selectbox('Stock', ticker_list)
 
-stock_data = yf.Ticker(selected_stock)
+	stock_data = yf.Ticker(selected_stock)
 
-df = stock_data.history(period='max')
-df.reset_index(inplace=True)
+	df = stock_data.history(period='max')
+	df.reset_index(inplace=True)
 
-fig = go.Figure([go.Scatter(x=df['Date'], y=df['High'])])
-fig.update_xaxes(
-    rangeslider_visible=True,
-    rangeselector=dict(
-        buttons=list([
-            dict(count=1, label="1m", step="month",
-                 stepmode="backward"),
-            dict(count=6, label="6m", step="month",
-                 stepmode="backward"),
-            dict(count=1, label="YTD", step="year",
-                 stepmode="todate"),
-            dict(count=1, label="1y", step="year",
-                 stepmode="backward"),
-            dict(step="all")
-        ])
-    )
-)
-st.plotly_chart(fig, use_container_width=True)
+	fig = go.Figure([go.Scatter(x=df['Date'], y=df['High'])])
+	fig.update_xaxes(
+		rangeslider_visible=True,
+		rangeselector=dict(
+			buttons=list([
+				dict(count=1, label="1m", step="month",
+					 stepmode="backward"),
+				dict(count=6, label="6m", step="month",
+					 stepmode="backward"),
+				dict(count=1, label="YTD", step="year",
+					 stepmode="todate"),
+				dict(count=1, label="1y", step="year",
+					 stepmode="backward"),
+				dict(step="all")
+			])
+		)
+	)
+	st.plotly_chart(fig, use_container_width=True)
