bfa637f04005c619ab7335963ed8f766d1971bdd
iamjigyanshu <78719108+iamjigyanshu@users.noreply.github.com>
2022-09-30 20:05:56 +0530

    Update login_test.py

--- Login_Test/login_test.py
+++ Login_Test/login_test.py
@@ -37,32 +37,34 @@ def view_all_users():
 	return data
 
 def analytics():
-    ticker_list = pd.read_csv('s&p500.csv')
-    selected_stock = st.sidebar.selectbox('Stock', ticker_list)
-    
-    stock_data = yf.Ticker(selected_stock)
-    
-    df = stock_data.history(period = 'max')
-    df.reset_index(inplace=True)
-    
-    fig = go.Figure([go.Scatter(x=df['Date'], y=df['High'])])
-    fig.update_xaxes(
-        rangeslider_visible=True,
-        rangeselector=dict(
-            buttons=list([
-                dict(count=1, label="1m", step="month",                                        
-                     stepmode="backward"),
-                dict(count=6, label="6m", step="month",  
-                     stepmode="backward"),
-                dict(count=1, label="YTD", step="year", 
-                     stepmode="todate"),
-                dict(count=1, label="1y", step="year", 
-                     stepmode="backward"),
-                dict(step="all")
-            ])
-        )
-    )
-    st.plotly_chart(fig, use_container_width=True)
+	
+	#s&p500.csv
+	ticker_list = pd.read_csv('s&p500.csv')
+	selected_stock = st.sidebar.selectbox('Stock', ticker_list)
+
+	stock_data = yf.Ticker(selected_stock)
+
+	df = stock_data.history(period = 'max')
+	df.reset_index(inplace=True)
+
+	fig = go.Figure([go.Scatter(x=df['Date'], y=df['High'])])
+	fig.update_xaxes(
+	rangeslider_visible=True,
+	rangeselector=dict(
+	    buttons=list([
+		dict(count=1, label="1m", step="month",                                        
+		     stepmode="backward"),
+		dict(count=6, label="6m", step="month",  
+		     stepmode="backward"),
+		dict(count=1, label="YTD", step="year", 
+		     stepmode="todate"),
+		dict(count=1, label="1y", step="year", 
+		     stepmode="backward"),
+		dict(step="all")
+	    ])
+	)
+	)
+	st.plotly_chart(fig, use_container_width=True)
 
 
 
