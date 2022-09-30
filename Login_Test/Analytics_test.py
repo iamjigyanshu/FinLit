3ff95cc92576dae6f849f22b2f29d4e8452cd027
iamjigyanshu <78719108+iamjigyanshu@users.noreply.github.com>
2022-10-01 03:56:52 +0530

    Update Analytics_test.py

--- Login_Test/Analytics_test.py
+++ Login_Test/Analytics_test.py
@@ -20,8 +20,6 @@ def app():
 	st.subheader("About the Company")
 	st.write(sentence)
 	
-	st.write("\n")
-	st.subheader("Price Chart")
 	fig = go.Figure([go.Scatter(x=df['Date'], y=df['High'])])
 	fig.update_xaxes(
 		rangeslider_visible=True,
