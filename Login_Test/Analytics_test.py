8de40ff29a9782b8b739c612ff872e2afa861bc8
iamjigyanshu <78719108+iamjigyanshu@users.noreply.github.com>
2022-10-01 04:44:13 +0530

    Update Analytics_test.py

--- Login_Test/Analytics_test.py
+++ Login_Test/Analytics_test.py
@@ -40,10 +40,29 @@ def app():
 	fig.update_layout(title_text="Closing Price Chart")
 	fig.update_layout(xaxis=dict(rangeselector = dict(font = dict( color = "black"))))
 	st.plotly_chart(fig, use_container_width=True)
-	
-	volume_chart = px.area(df['Volume'], 
-                            title='Stock Daily Volume', 
-                            color_discrete_map={'Volume':'red'} , 
-                            width=800, height=400)
-	st.plotly_chart(volume_chart, use_container_width=True)
+	for i in df['Date']:
+		if i == date:
+			ans = df.loc[df['Date'] == i].index[0]
+			break
+	one_year_date = df['Date'].iloc[ans:]
+	one_year_vol = df['Volume'].iloc[ans:]
+	vol_chart = go.Figure([go.Scatter(x=one_year_date, y=one_year_vol,line=dict(color="#FF0000"))])
+	vol_chart.update_xaxes(
+	    rangeslider_visible=True,
+	    rangeselector=dict(
+		buttons=list([
+		    dict(count=1, label="1m", step="month",                                        
+			 stepmode="backward"),
+		    dict(count=6, label="6m", step="month",  
+			 stepmode="backward"),
+		    dict(count=1, label="YTD", step="year", 
+			 stepmode="todate"),
+		    dict(count=1, label="1y", step="year", 
+			 stepmode="backward"),
+		    dict(step="all")
+		])
+	    )
+	)
+	vol_chart.update_layout(title_text="Daily Volume Chart")
+	st.plotly_chart(vol_chart, use_container_width=True)
 # 	volume_chart.show()
