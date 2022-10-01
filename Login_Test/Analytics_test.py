1af75844aac3daa4fc061e49d95704b66a3556ce
iamjigyanshu <78719108+iamjigyanshu@users.noreply.github.com>
2022-10-01 07:29:37 +0530

    Update Analytics_test.py

--- Login_Test/Analytics_test.py
+++ Login_Test/Analytics_test.py
@@ -54,23 +54,10 @@ def app():
 	one_year_date = df['Date'].iloc[ans:]
 	one_year_vol = df['Volume'].iloc[ans:]
 	vol_chart = go.Figure([go.Scatter(x=one_year_date, y=one_year_vol,line=dict(color="#FF0000"))])
-# 	vol_chart.update_xaxes(
-# 	    rangeslider_visible=True,
-# 	    rangeselector=dict(
-# 		buttons=list([
-# 		    dict(count=1, label="1m", step="month",                                        
-# 			 stepmode="backward"),
-# 		    dict(count=6, label="6m", step="month",  
-# 			 stepmode="backward"),
-# 		    dict(count=1, label="YTD", step="year", 
-# 			 stepmode="todate"),
-# 		    dict(count=1, label="1y", step="year", 
-# 			 stepmode="backward"),
-# 		    dict(step="all")
-# 		])
-# 	    )
-# 	)
+	vol_chart.update_xaxes(
+	    rangeslider_visible=True
+	)
 	vol_chart.update_layout(title_text="Daily Volume Chart")
-# 	vol_chart.update_layout(xaxis=dict(rangeselector = dict(font = dict( color = "black"))))
+
 	st.plotly_chart(vol_chart, use_container_width=True)
 
