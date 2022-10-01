a7cd749f02a0af6cdf7a30f85206f99333b7b3c8
iamjigyanshu <78719108+iamjigyanshu@users.noreply.github.com>
2022-10-01 07:00:36 +0530

    Update login_test.py

--- Login_Test/login_test.py
+++ Login_Test/login_test.py
@@ -50,37 +50,6 @@ def view_all_users():
 	return data
 
 
-# def analytics():
-# 	# s&p500.csv
-# 	ticker_list = pd.read_csv('s&p500.csv')
-# 	selected_stock = st.sidebar.selectbox('Stock', ticker_list)
-
-# 	stock_data = yf.Ticker(selected_stock)
-
-# 	df = stock_data.history(period='max')
-# 	df.reset_index(inplace=True)
-
-# 	fig = go.Figure([go.Scatter(x=df['Date'], y=df['High'])])
-# 	fig.update_xaxes(
-# 		rangeslider_visible=True,
-# 		rangeselector=dict(
-# 			buttons=list([
-# 				dict(count=1, label="1m", step="month",
-# 					 stepmode="backward"),
-# 				dict(count=6, label="6m", step="month",
-# 					 stepmode="backward"),
-# 				dict(count=1, label="YTD", step="year",
-# 					 stepmode="todate"),
-# 				dict(count=1, label="1y", step="year",
-# 					 stepmode="backward"),
-# 				dict(step="all")
-# 			])
-# 		)
-# 	)
-# 	st.plotly_chart(fig, use_container_width=True)
-
-
-
 def main():
 	"""FinLit"""
 
@@ -97,10 +66,6 @@ def main():
 
 		username = st.sidebar.text_input("User Name")
 		password = st.sidebar.text_input("Password",type='password')
-		if len(password) >=6:
-			boo = False
-		else:
-			st.warning("Password should contain 6 or more characters")
 		if st.sidebar.checkbox("Login"):
 			# if password == '12345':
 			create_usertable()
@@ -139,7 +104,7 @@ def main():
 		boo = True
 		while (boo):
 			new_password = st.text_input("Password",type='password')
-			if len(password) >= 6:
+			if len(new_password) >= 6:
 				boo = False
 			else:
 				st.warning("Password should contain 6 or more characters")
