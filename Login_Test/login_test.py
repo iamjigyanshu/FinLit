06dea174d67bd4ed6311cf79ef1843fee872addd
iamjigyanshu <78719108+iamjigyanshu@users.noreply.github.com>
2022-10-01 12:21:46 +0530

    Update login_test.py

--- Login_Test/login_test.py
+++ Login_Test/login_test.py
@@ -66,6 +66,7 @@ def main():
 		fig = px.scatter(df, x="gdpPercap", y="lifeExp", animation_frame="year", animation_group="country",
            	size="pop", color="continent", hover_name="country",
            	log_x=True, size_max=55, range_x=[100,100000], range_y=[25,90])
+		fig.update_layout(showgrid=False)
 		st.plotly_chart(fig, use_container_width=True)
 
 
@@ -76,6 +77,7 @@ def main():
 		px.scatter(df, x="gdpPercap", y="lifeExp", animation_frame="year", animation_group="country",
            	size="pop", color="continent", hover_name="country",
            	log_x=True, size_max=55, range_x=[100,100000], range_y=[25,90])
+		fig.update_layout(showgrid=False)
 		st.plotly_chart(fig, use_container_width=True)
 		username = st.sidebar.text_input("User Name")
 		password = st.sidebar.text_input("Password",type='password')
